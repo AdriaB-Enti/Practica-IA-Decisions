c614c8c74010b68146e5ece761629df6a460a205
PauBlanes_ENTI <paublanespardo@enti.cat>
2018-01-11 19:06:53 +0100

    coses goap

--- SDL_Decisions/src/GOAP.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "WorldState.h"
-#include "Action.h"
--- SDL_Decisions/src/Transition.cpp
+++
@@ -1,9 +0,0 @@
-#include "Transition.h"
-
-void EnoughGold() {
-	std::cout << "hola" << std::endl;
-}
-
-Transition::Transition(/*State _currState, State _nextState,*/ bool(*pt2Func)()) {
-	CheckCondition = pt2Func;
-}
